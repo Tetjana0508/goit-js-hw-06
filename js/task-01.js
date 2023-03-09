0ab2b7f4fe4b30037da1ad2d5b2ea23906e8edd8
Tetjana0508 <dancingtatyana@gmail.com>
2023-03-09 21:32:18 +0200

    ДЗ 6, завдання 1

--- js/task-01.js
+++ js/task-01.js
@@ -1,7 +1,20 @@
-const listEl = document.querySelector('ul#categories');
-console.log('listEl', listEl);
+// 1.
 
-const listLinksEl = listEl.querySelectorAll('.item');
-console.log('listLinksEl', listLinksEl);
+const listEl = document.querySelector('ul#categories');
+// console.log('listEl', listEl);
+
+const listLinksEl = listEl.querySelectorAll('li.item');
+// console.log('listLinksEl', listLinksEl);
 
 console.log(`Number of categories:`, listLinksEl.length);
+
+// 2.
+
+listLinksEl.forEach((listLinks) => {
+  const headerEl = listLinks.querySelector('h2');
+  const headerLinksEl = listLinks.querySelectorAll('li');
+
+  console.log(`Category: ${headerEl.textContent}`);
+  console.log(`Elements: ${headerLinksEl.length}`);
+});
+
