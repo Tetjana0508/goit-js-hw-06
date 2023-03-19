48dbeaa122f3fcd9b7d8e509301899722584c769
Tetjana0508 <dancingtatyana@gmail.com>
2023-03-19 10:19:08 +0200

    Завдання 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,19 @@
-function getRandomHexColor() {
+function getRandomHexColor() { /* Для генерации случайного цвета функция - getRandomHexColor */
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+/*Напиши скрипт, который изменяет цвета фона элемента <body> через инлайн стиль при клике на button.change-color и выводит значение цвета в span.color.*/
+
+const backgroundColorEl = document.querySelector('body'); /* ссылка на body */
+// console.log(backgroundColorEl);
+
+const chowColor = document.querySelector('.color'); /* ссылка на span */
+const buttonChangeColor = document.querySelector('.change-color'); /* ссылка на кнопку */
+
+buttonChangeColor.addEventListener('click', () => { /* вешаем прослушивание клика (addEventListener) на кнопку и при клике выполниется код */
+  backgroundColorEl.style.backgroundColor = getRandomHexColor();
+  chowColor.textContent = getRandomHexColor();
+})
+// console.log(buttonChangeColor);
